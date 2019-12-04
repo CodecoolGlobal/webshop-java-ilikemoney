a63a58ab80b26e4f4e643682da9c0a6f8009cbdd
Lux0n <gyuri1994@gmail.com>
2019-12-04 10:46:58 +0100

    pictures for products

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -58,21 +58,21 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Sock of Water Walking", 62.5f, "NK", "you can walk on water but gets terribly wet and uncomfortable", clothing, wizardly));
-        productDataStore.add(new Product("Boots of Reverse Flying", 124.5f, "NK", "You will feel upside down floating on the surface", clothing, wizardly));
-        productDataStore.add(new Product("Not water", 0, "S","Look, smells and tastes like water, but you know it's not water, neither us nor the cosmos know where it comes from", other, blibdoolpoolp));
-        productDataStore.add(new Product("Common fruit", 1, "S", "common", other, blibdoolpoolp));
-        productDataStore.add(new Product("Tie of Protection +1", 32, "NK", "Protects your tie from weapon damage", clothing, wizardly));
+        productDataStore.add(new Product("Sock of Water Walking", 62.5f, "NOK", "you can walk on water but gets terribly wet and uncomfortable", clothing, wizardly));
+        productDataStore.add(new Product("Boots of Reverse Flying", 124.5f, "NOK", "You will feel upside down floating on the surface", clothing, wizardly));
+        productDataStore.add(new Product("Not water", 0, "SOS","Look, smells and tastes like water, but you know it's not water, neither us nor the cosmos know where it comes from", other, blibdoolpoolp));
+        productDataStore.add(new Product("Common fruit", 1, "SOS", "common", other, blibdoolpoolp));
+        productDataStore.add(new Product("Tie of Protection +1", 32, "NOK", "Protects your tie from weapon damage", clothing, wizardly));
         productDataStore.add(new Product("Endless vacuum", 340, "HUF", "You can't deny the fact that nothing is either none or infinite", nickNack, tronley));
         productDataStore.add(new Product("Linked List", 12005, "HUF", "A pair of shopping lists tied together, you always know where the next one is when you hold either one", scroll, tronley));
         productDataStore.add(new Product("Actually Instant Noodles", 1200, "HUF", "Just a can of baked noodles, but I'm sure you'll love it!", food, tronley));
-        productDataStore.add(new Product("Rubber-duck of Silence", 44, "NK", "If you'd speak to it, your talk won't leave a noise", nickNack, wizardly));
+        productDataStore.add(new Product("Rubber-duck of Silence", 44, "NOK", "If you'd speak to it, your talk won't leave a noise", nickNack, wizardly));
         productDataStore.add(new Product("Suit of Mirage", 26515, "HUF", "The ones looking at you will experience a mirage of miracles", clothing, tronley));
         productDataStore.add(new Product("Chalice of Suffering", 469, "HUF", "It seriously hurts, don't touch it. What else do you have friends for?", cutlery, tronley));
         productDataStore.add(new Product("Helm of Sight", 11999.99f, "HUF", "Perfect vision no matter where are your eyes, if your eyes work properly, that is", clothing, tronley));
-        productDataStore.add(new Product("Helm of Insight", 2, "S", "Grants us eyes, Grant us eyes! By Kosm or some say Cos, You shall experience the cosmic truth", clothing, blibdoolpoolp));
+        productDataStore.add(new Product("Helm of Insight", 2, "SOS", "Grants us eyes, Grant us eyes! By Kosm or some say Cos, You shall experience the cosmic truth", clothing, blibdoolpoolp));
         productDataStore.add(new Product("Instant Skeletal Buzz", 590, "HUF", "You will feel your whole skeleton while you drink this miracle elixir. At least for a good second... also tastes like beer", potion, tronley));
-        productDataStore.add(new Product("Void Box", 1, "S", "It is not Void because it is empty, It is, because we do not know what it is full of", other, blibdoolpoolp));
-        productDataStore.add(new Product("Bag of Bag of Holding Holding", 112, "NK", "A bag of holding that holds bag of holdings", clothing, wizardly));
+        productDataStore.add(new Product("Void Box", 1, "SOS", "It is not Void because it is empty, It is, because we do not know what it is full of", other, blibdoolpoolp));
+        productDataStore.add(new Product("Bag of Bag of Holding Holding", 112, "NOK", "A bag of holding that holds bag of holdings", clothing, wizardly));
     }
 }
